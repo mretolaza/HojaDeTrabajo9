e9d0f95ddc7d1b0d1c77d26e4405ebd15e7cfc55
LuisDiegoFV <fer16344@uvg.edu.gt>
2017-05-24 10:03:17 -0600

    MiParteTerminada

--- HDT9_1/src/Manejador.java
+++ HDT9_1/src/Manejador.java
@@ -17,6 +17,32 @@ public class Manejador {
 
 	//private Conexion con = new Conexion();
 
-	
+	ArrayList<String> leerContenido(String archivo) {
+		String bfRead;
+		try {
+			BufferedReader ar = new BufferedReader(new FileReader(archivo));
+			while ((bfRead = ar.readLine()) != null) {
+				array.add(bfRead);
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "No se encontro archivo");
+		}
+		return array;
+	}
+
+	void hacergrafo(){
+		String ciudad1="",ciudad2="",distancia="";
+
+		for (String i:array){
+			int primero =i.indexOf(" ");
+			int segundo =i.indexOf(" ",primero+1);
+			int tercero =i.indexOf(" ",segundo+1);
+
+			ciudad1=i.substring(0,primero).trim();
+			ciudad2=i.substring(primero,segundo).trim();
+			distancia=i.substring(segundo,tercero).trim();
+		}
+		
+	}
 	
 }
